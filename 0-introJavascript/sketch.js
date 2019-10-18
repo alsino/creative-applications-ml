d507a8eb5a30eee82b05f9ede181f387736ade90
alsino <iamahorse@gmail.com>
2019-10-18 18:08:00 +0200

    Intro added

---
+++ 0-introJavascript/sketch.js
@@ -0,0 +1,12 @@
+// Variables (Numbers, strings)
+// Basic syntax (Console.log, Semicolons, sommas)
+// Arrays
+// Objects
+// Loops
+// Functions
+
+console.log("Hi! Let's start with the introduction to JavaScript");
+
+function setup(){
+  noCanvas();
+}
